8ba2ddd5b2062c5bdf247527cbf98bc266764bee
RoadToRight <syedsameershah0786@gmail.com>
2024-10-10 23:36:49 +0500

    Update dbConnection.js

--- Database/dbConnection.js
+++ Database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const dbConnection = () => {
 
-    mongoose.connect("mongodb+srv://syedsameershah0786:sameer@cluster0.fa5ds.mongodb.net/sameer?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("Connected To Database")).catch(() => console.log("Some Error Occured While Connecting To Database"))
+    mongoose.connect("fsd").then(() => console.log("Connected To Database")).catch(() => console.log("Some Error Occured While Connecting To Database"))
 
 }
 
