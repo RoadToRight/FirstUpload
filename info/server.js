c60fde582c8582baef60e02317f904ccdacb8040
RoadToRight <syedsameershah0786@gmail.com>
2024-10-10 23:02:57 +0500

    Update server.js

--- info/server.js
+++ info/server.js
@@ -2,7 +2,7 @@ import  app  from "./app.js";
 
 
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT || 4000,()=>{
 
     console.log(`Server is running at port ${process.env.PORT}`)
 
