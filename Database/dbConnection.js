c186ebc6fc8407ac672fabf8ac606bb553f9903d
RoadToRight <syedsameershah0786@gmail.com>
2024-10-11 07:49:23 +0500

    Update dbConnection.js

--- Database/dbConnection.js
+++ Database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const dbConnection = () => {
 
-    mongoose.connect("mongodb+srv://syedsameershah0786:sameer086@cluster0.fa5ds.mongodb.net/sam?retryWrites=true&w=majority&appName=Cluster0",{
+    mongoose.connect("mongodb+srv://syedsameershah0786:sameer0786@cluster0.fa5ds.mongodb.net/sam?retryWrites=true&w=majority&appName=Cluster0",{
         dbName:"Practiciny"
     }).then(() => console.log("Connected To Database")).catch(() => console.log("Some Error Occured While Connecting To Database"))
 
